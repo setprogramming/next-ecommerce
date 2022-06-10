7574adb8c349aa0ec17b4872cff668b533788caf
unknown <setprogramming@gmail.com>
2022-06-10 18:44:14 +0300

    added content to the Cart component

--- components/Cart.js
+++ components/Cart.js
@@ -4,10 +4,36 @@ import {AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping} from "r
 import {TiDeleteOutline} from "react-icons/ti"
 import toast from "react-hot-toast"
 import { useStateContext } from "../context/StateContext"
+import { urlFor } from "../lib/client"
 
 const Cart = () => {
+  const cartRef = useRef()
+  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button type="button" className="cart-heading" onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">
+            ({totalQuantities} items)
+          </span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping cart is empty</h3>
+            <Link href="/">
+              <button type="button" onClick={() => setShowCart(false)} className="btn">
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
